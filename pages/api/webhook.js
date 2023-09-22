1d0a4ad3fac86b3833d48a477af43b9f510d7a72
Adamch85 <140511473+Adamch85@users.noreply.github.com>
2023-09-22 12:51:46 -0400

    Update webhook.js

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,17 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
 import {createClient} from "@supabase/supabase-js";
 
 const supaclient = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+export default function handler(req, res) {
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
   try {
     if (req.method === "POST") {
 
-      let webhook_data = req.body.transaction;
+      let webhook_data = req.body.transaction
 
       const { data, error } = await supabase
         .from('txs')
