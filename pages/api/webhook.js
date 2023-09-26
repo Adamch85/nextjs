daab802b4b04bc18a6ac5a43ab0047daf57ddbf2
Adamch85 <140511473+Adamch85@users.noreply.github.com>
2023-09-26 04:54:02 -0400

    Update webhook.js

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,25 +1,48 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import * as anchor from "@coral-xyz/anchor";
+import {Connection, Keypair, PublicKey} from '@solana/web3.js';
 import {createClient} from "@supabase/supabase-js";
+const bs58 = require('bs58')
+
+const idl = require('./idl.json')
+const rpc = "https://devnet.helius-rpc.com/?api-key=42cac9b9-6eed-4413-8589-3ee3e2fbe321";
+const connection = new Connection(rpc);
+const keypair = Keypair.fromSecretKey(bs58.decode(process.env.SECRET_KEY))
+const wallet = new anchor.Wallet(keypair);
+const provider = new anchor.AnchorProvider(connection, wallet, {preflightCommitment: "processed"});
+const programID = new PublicKey("DfinCfYioVNG7UXFf1qxMS2Ycr9GrFBLBn9QPuHhnRUi");
+const program = new anchor.Program(idl, programID, provider);
+const eventParser = new anchor.EventParser(
+    program.programId,
+    program.coder
+);
+
 
 const supaclient = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 export default async function handler(req, res) {
 
-  try {
-    if (req.method === "POST") {
+    try {
+        if (req.method === "POST") {
 
-      let webhook_data = req.body[0]
-      let tx = webhook_data["transaction"]
+            let webhook_data = req.body
 
-     const { data, error } = await supaclient
-       .from('txs')
-       .upsert(tx)
-      res.status(200).json("success")
+            for (const tx of webhook_data) {
+                let events = eventParser.parseLogs(tx.meta?.logMessages)
+                for (let event of events) {
+                    switch (event.name) {
+                        case "NewRaffleEvent":
+                            const {data, error} = await supaclient
+                                .from('raffles')
+                                .upsert(event.data)
+                    }
+                }
+            }
 
-      console.log(data, error)
+            res.status(200).json("success")
 
-    };
+            // console.log(data, error)
 
-  }
+        }
 
-  catch (err) { console.log(err) }
+    } catch (err) { console.log(err) }
 }
